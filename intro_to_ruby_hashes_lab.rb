d612cbf56f13a03175de66a963034710789b2bd9
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-25 19:36:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,16 +36,39 @@ def monopoly_with_third_tier
   
   
   
-# - a `:reading_railroad` key that points to an empty hash
-# - a `:pennsylvania_railroad` key that points to an empty hash
-# - a `:b_and_o_railroad` key that points to an empty hash
-# - a `:shortline_railroad` key that points to an empty hash
-
   
     
 end
 
 def monopoly_with_fourth_tier
+    
+  trains = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned =>100, :four_pieces_owned =>200},
+      :name => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+  }
+  }
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
  
 
 end
