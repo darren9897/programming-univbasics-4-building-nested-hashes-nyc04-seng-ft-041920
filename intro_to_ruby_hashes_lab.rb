baf40cdbc45b5b0ddf408b615ced6ea3157b183b
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-25 19:35:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,17 @@ end
 
 def monopoly_with_third_tier
   
-  trains = {:railroads => {:pieces => 4,
-  :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned =>100, :four_pieces_owned =>200},
-  :name => {}
+  trains = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned =>100, :four_pieces_owned =>200},
+      :name => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+  }
   }
     
   }
